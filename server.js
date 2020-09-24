9990a6774f448a1955844498b67de95686213b6a
erezrokah <erezrokah@users.noreply.github.com>
2020-09-24 16:52:20 +0200

    chore: reproduce issue

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const startExternalServer = () => {
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.all('*', function (req, res) {
+    res.json({ url: req.url, body: req.body, method: req.method });
+  });
+
+  return app.listen(6000);
+};
+
+startExternalServer();
